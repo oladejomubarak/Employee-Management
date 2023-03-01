223f8090c7a7c2d8c139b7ba8384aa9bdc672354
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-01 04:51:22 +0000

    structuring the model layer

--- src/main/java/com/example/employeemanagement/model/Employee.java
+++ src/main/java/com/example/employeemanagement/model/Employee.java
@@ -1,14 +1,25 @@
 package com.example.employeemanagement.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+
     private String firstName;
     private String lastName;
     private String email;
 
+    public Employee(){
 
+    }
 
     public Employee(String firstName, String lastName, String email) {
+        super();
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
