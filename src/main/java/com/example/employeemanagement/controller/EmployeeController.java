15ea5abf929418002c71bbee8a942e71c432c12c
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-04 01:07:17 +0000

    creating list of employees method

--- src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.example.employeemanagement.controller;
 import com.example.employeemanagement.model.Employee;
 import com.example.employeemanagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3001/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
