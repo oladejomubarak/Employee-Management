1715bdead3fc2e59a7d95869ff566fa2adf9bf86
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-06 05:36:49 +0000

    add employee method implemented

--- src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.employeemanagement.controller;
 import com.example.employeemanagement.model.Employee;
 import com.example.employeemanagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +17,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployee(){
         return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee ){
+        return employeeRepository.save(employee);
     }
 }
